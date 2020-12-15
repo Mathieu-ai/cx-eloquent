b149c193382661d1b186ea22540d7b68e7e4a4d5
Mathieu Lièvre <mathieu.lievre@efrei.net>
2020-12-15 11:12:32 +0100

    adding ex.12

---
+++ ex12.js
@@ -0,0 +1,17 @@
+function perfect(number)
+{
+   let x = 0;
+
+   for(var i = 1; i <= number/2; i++) {
+         if(number % i === 0) {
+            x += i;
+         }
+   }
+   
+     if ( x === number && x !== 0) {
+       console.log("yes ( ´ ▽ ` )ﾉ ");
+      } else console.log("no ⸜(ّᶿധّᶿ)⸝ ");  
+} 
+ 
+console.log("is 6 a perfect number ? : ") + perfect(6);
+console.log("is 666 a perfect number ? : ") + perfect(666);
