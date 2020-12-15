3a598af0c8ca6fb7a82bed595234b8e042172a9e
Mathieu Lièvre <mathieu.lievre@efrei.net>
2020-12-15 11:29:38 +0100

    changed ex.09 \(-ㅂ-)/

--- ex09.js
+++ ex09.js
@@ -2,9 +2,9 @@ let whatType = (arg) => {
     return typeof arg;
 }
 function g () {
-    
+
 }
-console.log(whatType(42))
+console.log(whatType(666))
 console.log(whatType('hello'))
 console.log(whatType(true))
 console.log(whatType({}))
