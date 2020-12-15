c6bcce733bb70bebbdc8ea5498a141f1cfee143b
Mathieu Lièvre <mathieu.lievre@efrei.net>
2020-12-15 11:37:25 +0100

    changed ex.11 (☝ ՞ਊ ՞)☝

--- ex11.js
+++ ex11.js
@@ -1,7 +1,7 @@
-function getMax(array) {
+function getMinMax(array) {
     console.log(Math.max.apply(null, array));
     console.log(Math.min.apply(null, array));
 }
 const number = [1, 2, 3, 4, 5, 6, 7, 8,]
 
-return getMax(number)
+return getMinMax(number)
